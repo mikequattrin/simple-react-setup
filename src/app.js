b1666fa14b1f2b62c98d21b85f6dd1898af7dbc4
Mike Quattrin <mikequattrin@gmail.com>
2017-06-22 09:05:04 -0400

    Added a couple comments
    Exercise is complete. No further action is required

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+// Create the React application with some content.
 var App = React.createClass({
   render: function() {
     return <p>Hello World</p>;
   }
 });
 
+// Render the application to the DOM
 ReactDOM.render(
   <App />,
   document.getElementById('app')
